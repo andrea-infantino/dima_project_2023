17141ac9cf76dd03570a6a719dd094b2531d692f
Al3ssandro-create <56521211+Al3ssandro-create@users.noreply.github.com>
2024-01-28 11:25:07 -0600

    New Branch

--- lib/src/logic/authentication/login.dart
+++ lib/src/logic/authentication/login.dart
@@ -1,3 +1,4 @@
+import 'package:dima_project_2023/src/logic/authentication/link_google.dart';
 import 'package:dima_project_2023/src/widgets/dialog_windows.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -13,12 +14,13 @@ class LoginLogic {
           .signInWithEmailAndPassword(email: email, password: password)
           .then((value) =>
               Session.instance.setUser(value.user!.email!, value.user!.uid))
+          .then((value) => linkEmailGoogle())
           .then((value) => Achievements.init())
           .then((value) => Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => const PagesManager()),
               ));
-    } on FirebaseAuthException catch (e) {
+    } on FirebaseAuthException {
       _invalidCredentials(context);
     }
   }
