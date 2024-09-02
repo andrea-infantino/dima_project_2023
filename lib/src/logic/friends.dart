7bfc96b1f471c19aabf33ede1ef2c0f87f23db3e
InfantinoAndrea00 <andrea1.infantino@mail.polimi.it>
2024-09-02 12:25:17 -0500

    Bugfixing friends

--- lib/src/logic/friends.dart
+++ lib/src/logic/friends.dart
@@ -6,6 +6,7 @@ class FriendsLogic {
   static Future<void> addFriend(String email) async {
     DBsnapshot.instance.social.value["friends"]!.add(email);
     DBsnapshot.instance.social.notifyListeners();
+    DBsnapshot.instance.global.notifyListeners();
     friendsSetFriendsOf(
         Session.instance.uid, DBsnapshot.instance.social.value["friends"]!);
     deleteRequest(email);
@@ -50,6 +51,7 @@ class FriendsLogic {
   static Future<void> deleteRequest(String email) async {
     DBsnapshot.instance.social.value["requests"]!.remove(email);
     DBsnapshot.instance.social.notifyListeners();
+    DBsnapshot.instance.global.notifyListeners();
     String uid = Session.instance.uid;
     friendsSetRequestOf(uid, DBsnapshot.instance.social.value["requests"]!);
   }
